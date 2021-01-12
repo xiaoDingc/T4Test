72199e63b9d22471bb096d040e0b94567d6d07e1
dingdingc <d17521566034@gmail.com>
2021-01-12 18:55:10 +0800

    自定义过滤器用于跳过登录检查

--- T4模板程序/T4模板程序/Crane_Alarm_HisController.cs
+++ T4模板程序/T4模板程序/Crane_Alarm_HisController.cs
@@ -5,58 +5,58 @@ using Model;
 namespace T4模板程序
 {
 
-    //[SkipCheckLogin123]
-    public class Crane_Alarm_HisController //: Controller
-    {
+	[SkipCheckLogin]
+	public  class Crane_Alarm_HisController //: Controller
+	{
 
-        #region DI
+		#region DI
 
-        public Crane_Alarm_HisController(ICrane_Alarm_HisService crane_Alarm_His)
-        {
-            _crane_Alarm_His = crane_Alarm_His;
-        }
+		public Crane_Alarm_HisController(ICrane_Alarm_HisService crane_Alarm_His)
+		{
+			_crane_Alarm_His = crane_Alarm_His;
+		}
 
-        ICrane_Alarm_HisService _crane_Alarm_His { get; }
+		ICrane_Alarm_HisService _crane_Alarm_His { get; }
 
-        #endregion
+		#endregion
 
-        #region 获取
+		#region 获取
 
-        public async Task<PageResult<Crane_Alarm_His>> GetDataList(PageInput<Crane_Alarm_His> input)
-        {
-            return await _crane_Alarm_His.GetDataListAsync(input);
-        }
+		public async Task<PageResult<Crane_Alarm_His>> GetDataList(PageInput<Crane_Alarm_His> input)
+		{
+			return await _crane_Alarm_His.GetDataListAsync(input);
+		}
 
-        public async Task<Crane_Alarm_His> GetTheData(IdInputDTO input)
-        {
-            return await _crane_Alarm_His.GetTheDataAsync(input.id);
-        }
+		public async Task<Crane_Alarm_His> GetTheData(IdInputDTO input)
+		{
+			return await _crane_Alarm_His.GetTheDataAsync(input.id);
+		}
 
-        #endregion
+		#endregion
 
-        #region 提交
+		#region 提交
 
-        public async Task SaveData(Crane_Alarm_His data)
-        {
-            if (data.Id.IsNullOrEmpty())
-            {
-                InitEntity(data);
+		public async Task SaveData(Crane_Alarm_His data)
+		{
+			if (data.Id.IsNullOrEmpty())
+			{
+				InitEntity(data);
 
-                await _crane_Alarm_His.AddDataAsync(data);
-            }
-            else
-            {
-                await _crane_Alarm_His.UpdateDataAsync(data);
-            }
-        }
+				await _crane_Alarm_His.AddDataAsync(data);
+			}
+			else
+			{
+				await _crane_Alarm_His.UpdateDataAsync(data);
+			}
+		}
 
-        public async Task DeleteData(List<string> ids)
-        {
-            await _crane_Alarm_His.DeleteDataAsync(ids);
-        }
+		public async Task DeleteData(List<string> ids)
+		{
+			await _crane_Alarm_His.DeleteDataAsync(ids);
+		}
 
-        #endregion
+		#endregion
 
 
-    }
+	}
 }
